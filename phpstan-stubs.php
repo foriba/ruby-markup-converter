731c6d11ef405cde6312c23a6e7d1fcea9f28752
Foriba <f.river.2012@gmail.com>
2026-05-17 21:03:43 +0900

    Add PHPStan configuration

---
+++ phpstan-stubs.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * PHPStan stubs for Ruby Markup Converter.
+ *
+ * @package RubyMarkupConverter
+ */
+
+define( 'RUBYMACO_PLUGIN_DIR', __DIR__ . '/' );
+define( 'RUBYMACO_PLUGIN_URL', 'https://example.com/wp-content/plugins/ruby-markup-converter/' );
